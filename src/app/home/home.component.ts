aff511f7e4250b71a895fa069bd598afc1eeee6a
ViniciusApBrito <vinicius.brito355@gmail.com>
2023-06-24 00:40:50 -0300

    Deixando o projeto emperiquitado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { alunos } from '../alunos';
+import { AlunosService } from '../alunos.service';
+import { professores } from '../professores';
+import { ProfessoresService } from '../professores.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  alunos: alunos[] = [];
+  Professores: professores[] = [];
+
+  constructor(private alunosService: AlunosService, private professoresService: ProfessoresService) { }
+
+  ngOnInit(): void {
+    this.loadAlunos();
+    this.loadProfessores();
+  }
+  loadAlunos() {
+    this.alunosService.getAlunos().subscribe({
+      next: (data) => (this.alunos = data),
+    });
+  }
+
+loadProfessores() {
+    this.professoresService.getProfessores().subscribe({
+      next: (data) => (this.Professores = data),
+    });
+  }
 }
